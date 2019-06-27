60afddb2a651d419dfedc72c25e4b0419b14f8c3
eveento <evento@onet.eu>
2019-06-27 23:39:30 +0200

    dockercompose

--- SimpleClient/main.py
+++ SimpleClient/main.py
@@ -6,7 +6,7 @@ import random
 def main():
     print("Start")
 
-    number_of_threads = 1
+    number_of_threads = 4
     uuids = random.sample(range(1, 65535), number_of_threads)
     threads = []
     
